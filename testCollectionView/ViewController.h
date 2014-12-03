d5daa746e6f04c627b81b0f1d1f05ac7d9632afa
Vinay Raj <vinayraj.dunty@myntra.com>
2014-12-03 12:21:33 +0530

    Initial Commit

---
+++ testCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testCollectionView
+//
+//  Created by Vinay Raj Dunty on 03/12/14.
+//  Copyright (c) 2014 MyTectra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
